df37008aacb4374eb4ca9eed939815e0beb63238
Damian Grobler <54507680+Rikiflame01@users.noreply.github.com>
2024-12-03 15:02:03 +0200

    intro to waterscene

---
+++ Godzilla-urp-fix/Assets/!!Scripts/Player/ImageSequencePlayer.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ImageSequencePlayer : MonoBehaviour
+{
+    [Header("Settings")]
+    public Image displayImage;          
+    public Sprite[] imageArray;         
+    public int imagesPerSet = 3;        
+    public float imageDisplayTime = 1f; 
+    public float pauseDuration = 2f;    
+
+    private int currentIndex = 0;       
+    private bool isPlaying = true;
+
+    void Start()
+    {
+        if (imageArray.Length > 0 && displayImage != null)
+        {
+            StartCoroutine(PlayImageSequence());
+        }
+        else
+        {
+            Debug.LogWarning("Image array or displayImage is not set.");
+        }
+    }
+
+    private IEnumerator PlayImageSequence()
+    {
+        while (isPlaying)
+        {
+            for (int i = 0; i < imagesPerSet; i++)
+            {
+                if (currentIndex >= imageArray.Length)
+                {
+                    gameObject.SetActive(false);
+                }
+
+                displayImage.sprite = imageArray[currentIndex];
+                currentIndex++;
+                yield return new WaitForSeconds(imageDisplayTime);
+            }
+
+            yield return new WaitForSeconds(pauseDuration);
+        }
+    }
+
+    public void StopPlayback()
+    {
+        isPlaying = false;
+        StopAllCoroutines();
+    }
+
+    public void ResumePlayback()
+    {
+        if (!isPlaying)
+        {
+            isPlaying = true;
+            StartCoroutine(PlayImageSequence());
+        }
+    }
+}
